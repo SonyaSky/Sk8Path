31092e0329b070f459ad21d5b80b973aab723597
Sofya Sidonskaya <sonyasidonya@gmail.com>
2025-06-29 22:58:35 +0700

    approve and reject
    button for approving and rejecting apots and roads work

--- AdminPanel/Controllers/RoadController.cs
+++ AdminPanel/Controllers/RoadController.cs
@@ -40,5 +40,45 @@ namespace AdminPanel.Controllers
                 return View(new List<RoadDto>());
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var accessToken = Request.Cookies["AccessToken"];
+
+            if (string.IsNullOrEmpty(accessToken))
+                return RedirectToAction("Login", "Account");
+
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await _client.DeleteAsync($"http://95.182.120.75:8082/api/road/{id}/approve");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["Error"] = "Ошибка при удалении дороги.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reject(Guid id)
+        {
+            var accessToken = Request.Cookies["AccessToken"];
+
+            if (string.IsNullOrEmpty(accessToken))
+                return RedirectToAction("Login", "Account");
+
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var request = new HttpRequestMessage(HttpMethod.Put, $"http://95.182.120.75:8082/api/road/{id}/decline");
+
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["Error"] = "Ошибка при отклонении дороги.";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- AdminPanel/Controllers/SpotController.cs
+++ AdminPanel/Controllers/SpotController.cs
@@ -40,5 +40,45 @@ namespace AdminPanel.Controllers
                 return View(new List<SpotDto>());
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var accessToken = Request.Cookies["AccessToken"];
+
+            if (string.IsNullOrEmpty(accessToken))
+                return RedirectToAction("Login", "Account");
+
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await _client.DeleteAsync($"http://95.182.120.75:8082/api/spot/{id}/approve");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["Error"] = "Ошибка при удалении места.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reject(Guid id)
+        {
+            var accessToken = Request.Cookies["AccessToken"];
+
+            if (string.IsNullOrEmpty(accessToken))
+                return RedirectToAction("Login", "Account");
+
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var request = new HttpRequestMessage(HttpMethod.Put, $"http://95.182.120.75:8082/api/spot/{id}/decline");
+
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["Error"] = "Ошибка при отклонении места.";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
